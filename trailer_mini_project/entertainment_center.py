e05dc7e813d705bb4f2ffc93617d58c56d3b5b1b
Abhi-sigma <nonestate@gmail.com>
2017-07-12 23:56:18 +1000

    Add files via upload

--- trailer_mini_project/entertainment_center.py
+++ trailer_mini_project/entertainment_center.py
@@ -13,49 +13,41 @@ from fresh_tomatoes import open_movies_page
 # Fast and Furious 1,Trailer URL,Movie Image
 fast_and_furious_1 = Movies("Fast and Furious 1",
                             "https://www.youtube.com/watch?v=2TAOizOnNPo",
-                            "http://t1.gstatic.com/images?q=tbn:ANd9GcT18dHcbyb6cKFIjPsy6g8vgd-E6XfBcJr5NIBYAcmaP9T1wlfn")
-
+                            "http://t1.gstatic.com/images?q=tbn:ANd9GcT18dHcbyb6cKFIjPsy6g8vgd-E6XfBcJr5NIBYAcmaP9T1wlfn")  # noqa
 
 # Fast and Furious 2,Trailer URL,Movie Image
 fast_and_furious_2 = Movies("Fast and Furious 2",
                             "https://www.youtube.com/watch?v=ZZGkV_xWGw4",
-                            "http://t0.gstatic.com/images?q=tbn:ANd9GcTQ-0PNL4tdTxg58H50JJs_HIzPi3und4vRh3yOJLKTyZ8Wy3f0")
-
+                            "http://t0.gstatic.com/images?q=tbn:ANd9GcTQ-0PNL4tdTxg58H50JJs_HIzPi3und4vRh3yOJLKTyZ8Wy3f0")  # noqa
 
 # Fast and Furious 3,Trailer URL,Movie Image
 fast_and_furious_3 = Movies("Fast and Furious 3",
                             "https://www.youtube.com/watch?v=p8HQ2JLlc4E",
-                            "http://www.gstatic.com/tv/thumb/movieposters/159790/p159790_p_v8_ag.jpg")
-
+                            "http://www.gstatic.com/tv/thumb/movieposters/159790/p159790_p_v8_ag.jpg")  # noqa
 
 # Fast and Furious 4,Trailer URL,Movie Image
 fast_and_furious_4 = Movies("Fast and Furious 4", "https://www.youtube.com/watch?v=YXZjANEY6bA",
-                            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcReS9bG9ucbcpFGK0i-YFRBeT7KhBMgYQyU8Ttc9GNpcjBWGfKp")
-
+                            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcReS9bG9ucbcpFGK0i-YFRBeT7KhBMgYQyU8Ttc9GNpcjBWGfKp")  # noqa
 
 # Fast and Furious 5,Trailer URL,Movie Image
-fast_and_furious_5 = Movies("Fast and Furious 5", "https://www.youtube.com/watch?v=OqjeOYeG5_A",
-                            "http://t1.gstatic.com/images?q=tbn:ANd9GcR9Y8JHdp2nSaGCT7RfryzQtCLmJgDDkV77IHKa1hXnQpaDfpDX")
-
+fast_and_furious_5 = Movies("Fast and Furious 5",
+                            "https://www.youtube.com/watch?v=OqjeOYeG5_A",
+                            "http://t1.gstatic.com/images?q=tbn:ANd9GcR9Y8JHdp2nSaGCT7RfryzQtCLmJgDDkV77IHKa1hXnQpaDfpDX")  #noqa
 
 # Fast and Furious 6,Trailer URL,Movie Image
 fast_and_furious_6 = Movies("Fast and Furious 6", "https://www.youtube.com/watch?v=oc_P11PNvRs",
-                            "http://t2.gstatic.com/images?q=tbn:ANd9GcR4YVpIByidoaq4cAwBQ5QENYgteSTk6jL4AL_iMspQJqYiaFG6")
-
+                            "http://t2.gstatic.com/images?q=tbn:ANd9GcR4YVpIByidoaq4cAwBQ5QENYgteSTk6jL4AL_iMspQJqYiaFG6")  #noqa
 
 # Fast and Furious 7,Trailer URL,Movie Image
 fast_and_furious_7 = Movies("Fast and Furious 7", "https://www.youtube.com/watch?v=Skpu5HaVkOc",
-                            "http://t1.gstatic.com/images?q=tbn:ANd9GcReedjA2vJSO4_6GDpsI3PShvbRqfAAEv03qaJ9qOxtiLZX0Jx7")
-
+                            "http://t1.gstatic.com/images?q=tbn:ANd9GcReedjA2vJSO4_6GDpsI3PShvbRqfAAEv03qaJ9qOxtiLZX0Jx7")  #noqa
 
 # Fast and Furious 8,Trailer URL,Movie Image
 fast_and_furious_8 = Movies("Fast and Furious 8", "https://www.youtube.com/watch?v=NxhEZG0k9_w",
-                            "https://images-na.ssl-images-amazon.com/images/M/MV5BMjMxODI2NDM5Nl5BMl5BanBnXkFtZTgwNjgzOTk1MTI@._V1_UY1200_CR64,0,630,1200_AL_.jpg")
-
+                            "https://images-na.ssl-images-amazon.com/images/M/MV5BMjMxODI2NDM5Nl5BMl5BanBnXkFtZTgwNjgzOTk1MTI@._V1_UY1200_CR64,0,630,1200_AL_.jpg")  #noqa
 
 # creating a list of instances of movies which is passed to
 # function open_movies_pages in fresh_tomtoes.py script
-
 
 list_of_movies = [fast_and_furious_1, fast_and_furious_2,
                   fast_and_furious_3, fast_and_furious_4,
--- trailer_mini_project/media.py
+++ trailer_mini_project/media.py
@@ -1,12 +1,20 @@
 
 
 # a contructor to create instances of movies
+
+
 class Movies(
 ):
+	
+	"""takes name of a movie,movie trailer url
+	from youtube and url of a movie image as arguments
+	to create a instance of the movie"""
 
-    def __init__(self,
-                 title, trailer_youtube_url,
-                 poster_image_url):
-        self.trailer_youtube_url = trailer_youtube_url
-        self.poster_image_url = poster_image_url
-        self.title = title
+
+
+	def __init__(self,
+             title, trailer_youtube_url,
+             poster_image_url):
+	    self.trailer_youtube_url = trailer_youtube_url
+	    self.poster_image_url = poster_image_url
+	    self.title = title
